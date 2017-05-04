fa944a8b5fd2be712e192fe164b74bce00222977
Niels Walter <n.walter@milestone-consult.de>
2017-05-04 12:16:37 +0200

    using json kvpair toset option

--- DNC_WS/Startup.cs
+++ DNC_WS/Startup.cs
@@ -7,11 +7,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace DNC_WS
 {
     public class Startup
     {
+
+        public Startup(IHostingEnvironment env)
+        {
+
+            // adding json config file
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -19,7 +36,7 @@ namespace DNC_WS
             // add serices here -- auth - id ,,,,
             services.AddOptions();
             services.AddReinRaus(options => {
-                options.Nummer = "Drei";
+                options.Nummer = Configuration["ReinRaus:Nummer"];
                 });
         }
 
